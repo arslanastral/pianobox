5c9813d38ad3aca3ff4583e06b87db157bbdca5e
Arslan <arslanastral@outlook.com>
2021-05-07 23:13:12 +0800

    fixed audio samples

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -69,6 +69,8 @@ var instrument = SampleLibrary.load({
     "flute",
     "female-voice-aa",
     "female-voice-oo",
+    "females-voice-aa",
+    "females-voice-oo",
     "french-horn",
     "guitar-acoustic",
     "guitar-electric",
